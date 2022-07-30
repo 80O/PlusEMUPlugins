c993441202d90c5220883a50c3c12c413c6ee3e1
Placeholder <placeholder@domain.com>
2022-07-30 13:56:41 +0200

    Create solution

---
+++ PluginExample/PluginExample.cs
@@ -0,0 +1,11 @@
+﻿using Plus.Plugins;
+
+namespace PluginExample;
+
+public class PluginExample : IPlugin
+{
+    public void Start()
+    {
+        Console.WriteLine("This plugin has been started :)");
+    }
+}
---
+++ PluginExample/PluginExampleDefinition.cs
@@ -0,0 +1,12 @@
+﻿using Plus.Plugins;
+
+namespace PluginExample
+{
+    public class PluginExampleDefinition : IPluginDefinition
+    {
+        public string Name => "Example Plugin";
+        public string Author => "The General";
+        public Version Version => new(1, 0, 0);
+        public Type PluginClass => typeof(PluginExample);
+    }
+}
